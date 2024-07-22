8ae3c2519748c44c240a6797dd929ac271016257
Юрий <fedejkin@gmail.com>
2024-07-22 12:48:33 +0300

    Initial commit

---
+++ src/main/java/pro/sky/streamAPIOption2_8/StreamApiOption28Application.java
@@ -0,0 +1,13 @@
+package pro.sky.streamAPIOption2_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiOption28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamApiOption28Application.class, args);
+	}
+
+}
